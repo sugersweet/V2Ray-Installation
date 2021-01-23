8c2df2386dc708babb4f6c96d53c46da8d1e80a5
Administrator <admin@example.com>
2021-01-23 11:29:09 +0800

    add terraform ignore

---
+++ terraform.tf
@@ -0,0 +1,25 @@
+resource "alicloud_vpc" "vpc" {
+  name       = "tf_test_foo"
+  cidr_block = "172.16.0.0/12"
+}
+
+resource "alicloud_vswitch" "vsw" {
+  vpc_id            = alicloud_vpc.vpc.id
+  cidr_block        = "172.16.0.0/21"
+  availability_zone = "cn-hongkong-b"
+}
+resource "alicloud_security_group" "default" {
+  name = "default"
+  vpc_id = alicloud_vpc.vpc.id
+}
+
+resource "alicloud_security_group_rule" "allow_all_tcp" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "1/65535"
+  priority          = 1
+  security_group_id = alicloud_security_group.default.id
+  cidr_ip           = "0.0.0.0/0"
+}
